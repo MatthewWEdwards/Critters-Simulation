879aae2f488ad92f1cf887959a9bd96e8872d18a
rstehle <regan.stehle@utexas.edu>
2016-10-19 23:59:56 -0500

    added header & javadoc comments

--- src/assignment4/Algae.java
+++ src/assignment4/Algae.java
@@ -12,20 +12,30 @@
 package assignment4;
 
 /*
- * Algae class
+ * Algae class, food course for other critters
  */
-public class Algae extends Critter {
+public class Algae extends Critter.TestCritter {
 
 	
 	@Override
+	/**
+	 * Algae represented in worldview by @
+	 */
 	public String toString() { return "@"; }
 	
 	@Override
+	/**
+	 * Behavior of Algae, increases energy by photosynthesis_energy, does not move
+	 */
 	public void doTimeStep() {
+		super.setEnergy(super.getEnergy() + Params.photosynthesis_energy_amount);
 		
 	}
 
 	@Override
+	/**
+	 * Behavior in a fight, does not elect to fight, does not try to flee
+	 */
 	public boolean fight(String opponent) {
 		return false;
 	}
